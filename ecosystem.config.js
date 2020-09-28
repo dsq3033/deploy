c148d3eb86b565bef40a75161463cd30eabb591f
董士奇 <dongshiqi@192.168.0.101>
2020-09-28 17:35:32 +0800

    xx

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,8 +12,8 @@ module.exports = {
     }],
     "deploy" : {
         "production" : {
-            user : "dsq", //目标服务器的登录用户名
-            host : ["192.168.137.17"],  //目标服务器地址
+            user : "dongshiqi", //目标服务器的登录用户名
+            host : ["192.168.0.101"],  //目标服务器地址
             ref  : "origin/master",  // 部署目标分支
             repo : "https://github.com/dsq3033/deploy.git", //git仓库地址
             "ssh_options" : 'StrictHostKeyChecking=no',  //ssh 登录校验
